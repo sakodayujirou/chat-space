f0970c3b8e5408fd63b189f2961a2bed4795d763
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-05-28 17:25:50 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -39,16 +39,10 @@ ActiveRecord::Schema.define(version: 20190504105101) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "e_mail",                 default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["e_mail"], name: "index_users_on_e_mail", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string   "name",       null: false
+    t.string   "e_mail",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "groups_users", "groups"
