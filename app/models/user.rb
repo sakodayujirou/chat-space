17e22da09a5b75ed7134c128c01ea877d033f77a
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-05-28 17:41:02 +0900

    データベース整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_many :groups_users
-  has_many :groups, through: :groups_users
-  has_many :messages
-
 end
